8ca328b560259f77f6c0365843b9664d8264c4c6
Ayrton Soto <ayrtonxd.123@gmail.com>
2021-02-18 19:44:44 -0500

    Agregada validacion de formulario

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+//modals
+mdlOclusion = document.getElementsByClassName('oclusioner');
+
+//botones
+btnCerrarAlerta = document.getElementById('btnCloseAlert');
+
 // Contenedor de tareas
 taskContainer = document.getElementsByClassName('taskcontainer');
+
+//elementos Alertas
+msgAlerta = document.getElementById("mensajeAlerta");
 
 // Inputs del form
 tareasForm = document.getElementById('tareasForm');
@@ -18,8 +27,12 @@ icons1 =  document.getElementsByClassName('bi-chevron-compact-right');
 
 
 //eventos
+btnCerrarAlerta.addEventListener('click', cerrarAlerta);
+
+cboCategoria.addEventListener('change', changeCbo);
+
 for (const chevron of icons1) {
-  chevron.addEventListener('click', (e)=>{
+  chevron.addEventListener('click', function(e){
     let iconsClasses = ["bi-chevron-compact-right","bi-chevron-compact-down"]
     if(e.target.classList[1] == iconsClasses[0]){
       e.target.classList.replace(iconsClasses[0],iconsClasses[1]);
@@ -33,16 +46,22 @@ for (const chevron of icons1) {
 
 //funciones
 function agregarTarea(){
-  console.log("agregando");
+  console.log("agregando...");
   nombre = txtnombreTarea.value;
   tf = dtFechaFin.value.split('-');
   fecha = `${tf[2]}/${tf[1]}/${tf[0]}`;
   tiempoInicio = timeInicio.value.toString();
   tiempoFin = timeFin.value.toString();
   categoria = cboCategoria.selectedOptions[0].innerText;
-  descripcion = tAreaDescripcion.value;
+  descripcion = procesarValueTextArea(tAreaDescripcion.value);
+  
+  // verficaciones;
 
-  crearComponente(createJSON(nombre,fecha,timeInicio,timeFin,categoria,descripcion));
+
+  if(verificarText(txtnombreTarea) * verificarText(dtFechaFin) * verificarText(timeInicio) * verificarText(timeFin) * verificarCbo(cboCategoria) *  verificarText(tAreaDescripcion)){
+    crearComponente(createJSON(nombre,fecha,tiempoInicio,tiempoFin,categoria,descripcion));
+    tareasForm.reset();  
+  }
 }
 
 function createJSON(nombre, fecha, tInicio, tFin, categoria, descripcion){
@@ -61,6 +80,7 @@ function createJSON(nombre, fecha, tInicio, tFin, categoria, descripcion){
 }
 
 function crearComponente(tarea){
+  tareasForm.reset();
   eHTML = taskContainer[0]
   let nElem = eHTML.childElementCount;
   eHTML.innerHTML += `
@@ -85,4 +105,70 @@ function crearComponente(tarea){
             </div>
           </div>
   `
+}
+
+function verificarCbo(cboElement){
+  console.log(cboElement.selectedIndex)
+  if(cboElement.selectedIndex==0){
+      console.log("Cuidado!!!")
+      cboElement.classList.add('bordeRojo');
+      document.getElementById('advCbo').classList.remove('oculto');
+      return 0
+  }else{
+    return 1;
+  }
+}
+
+function verificarText(inputTextElement){
+  console.log(inputTextElement);
+  if(inputTextElement.value!=""){
+    if(inputTextElement.classList.contains('bordeRojo')){
+      inputTextElement.classList.remove('bordeRojo');
+    }
+    console.log("valido");
+    return 1;
+  }else{
+    inputTextElement.classList.add('bordeRojo');
+    console.log("invalido");
+    return 0;
+  }
+}
+
+function changeCbo(e){
+  let htmlE = e.target;
+  console.log(htmlE);
+  if(htmlE.selectedIndex!=0){
+    if(htmlE.classList.contains('bordeRojo')){
+      console.log(e.target)
+      htmlE.classList.remove('bordeRojo');
+      if (!document.getElementById('advCbo').classList.contains('oculto'))
+      {document.getElementById('advCbo').classList.add('oculto');}
+    }
+  }
+}
+
+function lanzarAlerta(){
+
+}
+
+function procesarValueTextArea(jsText){
+  let htmlText = "";
+  lineas = jsText.split('\n');
+  for(let i = 0; i<lineas.length;i++){
+    if(i!=lineas.length-1){
+      htmlText+= lineas[i]+"<br>";
+    }else{
+      htmlText+= lineas[i];
+    }
+  }
+}
+
+function cerrarAlerta(e){
+  console.log("Funciona");
+  console.log(e);
+  for (const oclusioner of mdlOclusion) {
+    if (!oclusioner.classList.contains('oculto')){
+      oclusioner.classList.add('oculto')
+    }
+  }
 }
