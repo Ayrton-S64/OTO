0bfa4c453e431dceb17bf8b05d3f2adec4225703
Ayrton Soto <ayrtonxd.123@gmail.com>
2021-02-24 18:19:48 -0500

    agregado control credenciales

---
+++ conexion.php
@@ -0,0 +1,7 @@
+<?php 
+  function conectar(){
+    $conn_string = 'dbname=da14tgvhr874p2 host=ec2-52-22-161-59.compute-1.amazonaws.com port=5432 user=rzlfddaekpdauw password=104c50531b81f50affca244b773a6f428079c01ca21d9cc61e0274c5d25c9cba sslmode=require';
+    $dbconn = pg_connect($conn_string) or die('Could not connect: '. pg_last_error());
+    return $dbconn;
+  }
+?>
