409db2893471e52b020050ba1d732e51311751ac
Ayrton Soto <ayrtonxd.123@gmail.com>
2021-02-24 18:15:11 -0500

    cambios en conexion

--- dbTest.php
+++ dbTest.php
@@ -1,7 +1,9 @@
 <?php
+  require_once('./conexion.php');
+
   echo "Testing...";
-  $conn_string = 'dbname=da14tgvhr874p2 host=ec2-52-22-161-59.compute-1.amazonaws.com port=5432 user=rzlfddaekpdauw password=104c50531b81f50affca244b773a6f428079c01ca21d9cc61e0274c5d25c9cba sslmode=require';
-  $dbconn = pg_connect($conn_string) or die('Could not connect: '. pg_last_error());
+  
+  $con = conectar();
   $query =  'SELECT * FROM CATEGORIAS';
   $result = pg_query($query) or die('La consulta fallo: '.pg_last_error());
   
