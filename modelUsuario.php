f1443dc800048da42704f84e0899b38a48135cb5
Ayrton Soto <ayrtonxd.123@gmail.com>
2021-02-24 22:56:39 -0500

    terminado coltrolador usuario

--- modelUsuario.php
+++ modelUsuario.php
@@ -49,8 +49,22 @@
 
       session_start();
       $con = conectar();
-      $result = pg_query('INSERT INTO ');
-
+      $consulta = pg_query('SELECT * FROM USUARIOS WHERE nombre_usuario='.$user.';');
+      if(pg_num_rows($consulta)==0){
+        $result = pg_query('INSERT INTO USUARIOS(nombre_usuario, clave, telefono, correo) VALUES ('.$user.', '.$clave.', '.$celular.', '.$correo.');');
+        if(!$result){
+          $resp = array('success'=>0,'mensaje'=>'Surgió un problema intentalo mas tarde');
+        }else{
+          if(pg_affected_rows($result)==1){
+            $resp = array('success'=>1,'mensaje'=>'Registro exitoso');
+          }else{
+            $resp = array('success'=>0, 'mensaje'=>'No se afectaron ninguna fila?');
+          }
+        }
+      }else{
+        $resp = array('success'=>0,'mensaje'=>'Este usuario ya existe');
+      }
+      return json_encode($resp);
     }
   }
 ?>
