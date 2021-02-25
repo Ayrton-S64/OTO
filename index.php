d41802aab379b8dbcfc981439f99bec52195a70b
Ayrton Soto <ayrtonxd.123@gmail.com>
2021-02-25 11:05:28 -0500

    agregado redirección

--- index.php
+++ index.php
@@ -8,7 +8,12 @@
   <link rel="stylesheet" href="styles.css">
   <title>OTO</title>
 </head>
-<body>
+  <?php
+  session_start();
+    if(!isset($_SESSION['user'])){
+      header('location: ./login.html');
+    }
+  ?>
   <nav class="navbar navbar-expand-sm navbar-light bg-light">
     <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
         aria-expanded="false" aria-label="Toggle navigation">
@@ -213,7 +218,7 @@
       </div>
     </div>
   </div>
-  
+
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>  
