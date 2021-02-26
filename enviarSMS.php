90a39264c19ffd02c31633cc6574ec4979377151
Ayrton Soto <ayrtonxd.123@gmail.com>
2021-02-26 00:18:51 -0500

    testing

--- enviarSMS.php
+++ enviarSMS.php
@@ -1,6 +1,6 @@
 <?php
   $url = 'https://a6a748bf-203f-4af8-abb9-6e6295bafaef:Wa9jklJkXzMrEYD4gAvzpA@api.blower.io' . "/messages";
-  $data = array('to' => '+51933856134', 'message' => 'Hello from Blower.io');
+  $data = array('to' => '+51 933856134', 'message' => 'Hello from Blower.io');
   
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_POST, 1);
