b887cfa05ab6c3975b25327c11171c3b16282cf6
Ayrton Soto <ayrtonxd.123@gmail.com>
2021-02-26 03:23:49 -0500

    posiblemente sms listos

--- enviarSMS.php
+++ enviarSMS.php
@@ -34,7 +34,7 @@ $sDestination = '51'.$fila['telefono'].'';
 //$sDestination = array('346xxxxxxxx','346yyyyyyyy');
 $mensaje = "Tienes las siguientes tareas pendientes:\n";
 echo "SELECT * FROM tareas WHERE estado=1 AND id_usuario=".$_SESSION['user_id'].";";
-$result = pg_query("SELECT * FROM tareas WHERE estado=1 AND id_usuario=".$_SESSION['user_id'].";");
+$result = pg_query("SELECT * FROM tareas WHERE estado=1 AND id_usuario=".$_SESSION['user_id']." LIMIT 1;");
 $tempFecha = "";
 while($fila = pg_fetch_array($result)){
    if($fila['fecha']!=$tempFecha){
